56f19d39d9a65d82939e93d30b64ae062969ba6b
aticina <115451545+aticina@users.noreply.github.com>
2025-03-05 22:45:37 +0100

    wip

--- PZI_projekt/script.js
+++ PZI_projekt/script.js
@@ -376,121 +376,156 @@ applyButton.addEventListener("click", () => {
 */
 
 //START DATE
-const datepicker = document.querySelector(".start-datepicker");
-const dateInput = document.querySelector(".start-date-input");
-dateInput.addEventListener("click", () => { datepicker.hidden = false; });
+const startDatepicker = document.querySelector(".start-datepicker"); //div that contains bulk of datepicker, initially hidden, so everything except label and input field are in this
+const startDateInputElement = document.querySelector(".start-date-input");//gets input element aka square we write in
+startDateInputElement.addEventListener("click", () => { startDatepicker.hidden = false; });//when user clicks input field, div.start-datepicker gets unhidden
+//element.hidden = false/true, this sets value of hidden attribute, if it's true then element is hidden, if its false then its unhidden
+//MAYBE make it so when start is unhidden, end goes into hidding and other way around too?
 
-const cancelButton = document.querySelector(".start-cancel");
-cancelButton.addEventListener("click", () => { datepicker.hidden = true });
 /*
-const applyButton = document.querySelector(".start-apply");
-applyButton.addEventListener("click", () => { 
-  //show date in input window
-  dateInput.value = selectedDate.toLocaleDateString({ year:"numeric", month:"2-digit",day:"2-digit" });
-  datepicker.hidden = true })
+const startCancelButton = document.querySelector(".start-cancel");
+startCancelButton.addEventListener("click", () => { startDatepicker.hidden = true }); //closes startDatepicker aka hides it */
 
-  const dates = document.querySelector(".start-dates");
-  let selectedDate = new Date();
-  let year = selectedDate.getFullYear();
-  let month = selectedDate.getMonth();
 
-  function handleDateClick(e){
-    const button = e.target;
-    //remove selected state from previously selected
-    const selected = dates.querySelector(".start-selected");
-    selected && selected.classList.remove("start-selected");
-    //add selected class to selected date/buton
-    button.classList.add("start-selected");
-    selectedDate = new Date(year,month,parseInt(button.textContent));
-  }
+let startSelectedDate = new Date(); //this is current date aka today by definition of new Date(), but it will likely change to be whatever users sets it as cause
+// it is the date apply gets from input field
+let startYear = startSelectedDate.getFullYear();
+let startMonth = startSelectedDate.getMonth();
 
+
+const startApplyButton = document.querySelector(".start-apply");
+startApplyButton.addEventListener("click", () => { 
   
-  //month selector and year toggle when using prev and next buttons
-  const yearInput = document.querySelector(".start-year-input");
-  const monthInput = document.querySelector(".start-month-input");
+  startDateInputElement.value = startSelectedDate.toLocaleDateString({ year:"numeric", month:"2-digit",day:"2-digit" });
+  console.log(startDateInputElement.value);//I think value is string cause input type=text
+  //startDatepicker.hidden = false;
+ }); //IT PUTS DATE IN INPUT FIELD IN STRING FORM, it defaults at current date cause startSelectedDate=new date()
+  //it takes value from imput field via startDateInputElement.value and turns it into string form & closes/hides satrtdatepicker same way close button does
+  //first value it reads is an empty string cause nothing was in the field
 
-  function updateYearAndMonth(){
-    monthInput.selectedIndex = month;
-    yearInput.value = year;
+
+//we need this funtion for startDisplayDates
+//month selector and year toggle when using prev and next buttons
+const startYearInput = document.querySelector(".start-year-input"); //gets eleemnt that should show current year
+const startMonthInput = document.querySelector(".start-month-input"); //gets element that should show current month
+
+
+const startDates = document.querySelector(".start-dates");//gets div with nothing inside, after ??? gets button elements inside, each one is one date in calendar
+//fills dic.start-dates, makes date fields, each is a button
+function startDisplayDates(){
+
+  startUpdateYearAndMonth();//it returns nothing, just sets up that current month and year fields that are above date buttons
+  //prob does not need to be in here, just needs to be called right before to we can correctlly display current calendar page
+
+  startDates.innerHTML=""; //we are accessing div.start-dates innerHTML, initially it is empty
+
+  //for last week of prev month
+  const startLastDateOfPrevMonth = new Date(startYear,startMonth,0); //year and month are gotten from current datt from startSelcedtedDate variable,
+  // THIS IS SPECIFIC CRAFTED DATE, NOT CURRENT ONE, we need it to make that first line in caledar that is actually
+  //last days of previous month, they are muted/dull compared to toher dates
+  for(let i = 0; i <= startLastDateOfPrevMonth.getDay(); i++){
+    const startText = startLastDateOfPrevMonth.getDate() - startLastDateOfPrevMonth.getDay() + i;//calculating what number to put in a button/date field
+    const startButton = startCreateButton(startText,true,false); //CALLS ANOTHER FUNCTION TO MAKE BUTTON/DATE
+    //so we are sending a number, TRUE to make all these buttons.DISABLED(for that geryish/dull,unusable quality and looks),FALSE cause not one of these will be CURRENT/TODAY DATE
+    startDates.appendChild(startButton); //when we get made date/button it gets appended to div.start-dates container
   }
 
-  //makes all dates that need to be visible, visible and in right order
-  function displayDates(){
-    updateYearAndMonth(); 
-    //clear content
-    dates.innerHTML="";
-    //for last week of prev month
-    const lastDateOfPrevMonth = new Date(year,month,0);
-    for(let i = 0; i <= lastDateOfPrevMonth.getDay(); i++){
-      const text = lastDateOfPrevMonth.getDate() - lastDateOfPrevMonth.getDay() + i;
-      const button = createButton(text,true,false);
-      dates.appendChild(button);
-    }
+  //for current month(whole), it "knows" when to use 30 or 31
+  const startLastDateOfMonth = new Date(startYear,startMonth+1,0); //once again, speficic crafted date using current year and month from startSelectedDate
+  for(let i = 1; i <= startLastDateOfMonth.getDate(); i++){
 
-    //for current month(whole), it "knows" when to use 30 or 31
-    const lastDateOfMonth = new Date(year,month+1,0);
-    for(let i = 1; i <= lastDateOfMonth.getDate(); i++){
+    const startIsToday = startSelectedDate.getDate() === i && startSelectedDate.getFullYear() === year && startSelectedDate.getMonth() === month;
+    //calculates current date using startDelectedDate=new Date() which gives current date, with this and i it will mark one of dates as today 
+    //to mark it is will set startIsToday = TRUE, otherwise it is FALSE, this goes to CREATEBUTTON function that needs this info
+    const startButton = startCreateButton(i,false,startIsToday); //2nd argument is FALSE so not one button will be disabled, all will be ABLED
+    //creates button that we will append to all other dates/buttons, but first!
+    //each button gets their own eventlistener to react when they are clicked on
+    startButton.addEventListener("click",startHandleDateClick); //this does not actiavte them right away
 
-      //marking today's date visually
-      const isToday = selectedDate.getDate() === i && selectedDate.getFullYear() === year && selectedDate.getMonth() === month;
-
-      const button = createButton(i,false,isToday);
-      
-      button.addEventListener("click",handleDateClick);
-
-      dates.appendChild(button);
-    }
-
-    //for fisrt week of next month
-    const firstDateOfNextMonth = new Date(year,month+1,1);
-    for(let i = firstDateOfNextMonth.getDay(); i < 7; i++){
-      const text = firstDateOfNextMonth.getDate() - firstDateOfNextMonth.getDay() + i;
-      const button = createButton(text,true,false);
-      dates.appendChild(button);
-    }
+    startDates.appendChild(startButton);
   }
 
-  function createButton(text,isDisabled = false ,isToday = false ){
-    const button = document.createElement("button");
-    button.textContent = text;
-    button.disabled = isDisabled;
-    button.classList.toggle("start-today",isToday);
-    return button;
+  //for fisrt week of next month
+  const startFirstDateOfNextMonth = new Date(year,month+1,1);
+  for(let i = startFirstDateOfNextMonth.getDay(); i < 7; i++){
+    const startText = startFirstDateOfNextMonth.getDate() - startFirstDateOfNextMonth.getDay() + i;
+    const startButton = startCreateButton(startText,true,false);
+    //we want thes button disabeled too so we are sending TRUE as 2nd arg, FALSE for 3rd cause none of these will be today/current
+    startDates.appendChild(startButton);
   }
+}
 
-  displayDates();
+//NEEDED FOR STARTDISPLAYDATES() cause it makes every button able to get clicked on
+function startHandleDateClick(e){
+  const startButton = e.target; //targets one of buttons, one that's clicked
+  //remove selected state from previously selected
+  const startSelectedButton = startDates.querySelector(".start-selected"); //startSelectedButton is the button that's marked as selected
+  startSelectedButton && startSelectedButton.classList.remove("start-selected"); //it's no longer selected
+  //add selected class to selected date/buton
+  startButton.classList.add("start-selected"); //new one we clicked becomes selected
+  startSelectedDate = new Date(startYear,startMonth,parseInt(startButton.textContent));//once clicked we change this current date into something else
+}
 
-  //previous and next functionality
-  const prevButton = datepicker.querySelector(".start-prev");
-  const nextButton = datepicker.querySelector(".start-next");
+startDisplayDates(); //call it so it works
 
-  nextButton.addEventListener("click", () => {
-    if(month === 11){
-      year++;
-    }
-    month = (month + 1) % 12;
-    displayDates();
-  })
+//FUNTION THAT CREATES DATES/BUTTONS, needed for startDisplayDates()
+function startCreateButton(startText,startIsDisabled = false ,startIsToday = false ){
+  //receives NUMBER to put on a button/date, receives bool values or takes default ones for startIsDisabled that gives bool value to determine if button should be disabled
+  //startIsToday is also bool
+  const startButton = document.createElement("BUTTON");
+  startButton.setAttribute("type","button"); //this should be defualt for all buttons so I'll add it cause so is advised
+  startButton.textContent = startText; //sets NUMBER on BUTTON
+  startButton.disabled = startIsDisabled; //if true button will be disabled aka will get duller,greyish finish and we won't be able to click it, it is a button, but NOT USABLE
+  //by default of this function buttons are NOT DISABLED
+  startButton.classList.toggle("start-today",startIsToday); //by default we DON'T MARK BUTTON AS TODAY'S DATE, "start-today is class hta make sbutton black"
+  //mysterious 2nd argument for toggle is force, if TRUE it will only add new class, if FALSE it will only remove it
+  return startButton;//returns button that will be added to div.start-dates container
+}
 
-  prevButton.addEventListener("click", () => {
-    if(month === 0){
-      year--;
-    }
-    month = (month - 1 + 12) % 12;
-    displayDates();
-  })
+function startUpdateYearAndMonth(){ //this function sets current month and year correctlly based on two variables-startMonth and startYear
+  //both variables are let and defined above this function, they use startSelectedDate = new Date() aka current date as means of getting their info
+  startMonthInput.selectedIndex = startMonth;
+  startYearInput.value = startYear;
+}
 
-  //changing month changes looks of entire calendar
-  monthInput.addEventListener("change", () => {
-    month = monthInput.selectedIndex;
-    displayDates();
-  })
 
-  //changing year changes entier calendar displayed, like it should to make sense
-  yearInput.addEventListener("change", () => {
-    year = yearInput.value;
-    displayDates();
-  })
+
+
+
+//field to change months
+startMonthInput.addEventListener("change", () => { //listens to an element
+  startMonth = startMonthInput.selectedIndex; //changes month from current to what user selects
+  startDisplayDates(); //triggers displaydates cause they need refreshing
+});
+
+//field to change year
+startYearInput.addEventListener("change", () => {
+  startYear = startYearInput.value;
+  startDisplayDates();
+});
+
+
+//previous and next functionality
+const startPrevButton = startDatepicker.querySelector(".start-prev");
+const startNextButton = startDatepicker.querySelector(".start-next");
+
+startNextButton.addEventListener("click", () => {
+  if(startMonth === 11){
+    startYear++;
+  }
+  startMonth = (startMonth + 1) % 12;
+  startDisplayDates();
+});
+
+startPrevButton.addEventListener("click", () => {
+  if(startMonth === 0){
+    startYear--;
+  }
+  startMonth = (startMonth - 1 + 12) % 12;
+  startDisplayDates();
+});
+
+
   
 /*
 //END DATE
